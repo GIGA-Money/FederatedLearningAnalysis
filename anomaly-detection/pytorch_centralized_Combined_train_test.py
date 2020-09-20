448b2c0a2ea37dcc206b3efdb0f915f6087f7f46
GIGA-Money <gigachris1@hotmail.com>
2020-09-20 15:39:04 -0400

    more confusion matrix adjusting

--- anomaly-detection/pytorch_centralized_Combined_train_test.py
+++ anomaly-detection/pytorch_centralized_Combined_train_test.py
@@ -91,7 +91,7 @@ def printing_press(Y_pred, Y_test):
                                         title="Centralized Test",
                                         text_fontsize="large",
                                         true_labels=['benign', 'attack'],
-                                        pred_labels=['benign', 'attack'])
+                                        pred_labels=['attack', 'benign'])
     plt.savefig(
         f"figures/centralized/centralizedConfusionMatrix_{FLAGS.Input_dim}_{FLAGS.Learn_rate}_{FLAGS.Epochs}_{FLAGS.Batch_size}.png")
 
